56442d06b9ca8514a85340bd16c8d52f2ed52a9c
Mohit <mahajanmahi729@gmail.com>
2025-09-17 00:31:21 +0530

    ArrayList of city added here

---
+++ LearningGit/src/com/practice/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package com.practice;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Indore");
+		al.add("Pune");
+		al.add("Mumbai");
+		al.add("Banglore");
+		
+		System.out.println(al);
+		
+		
+		
+	}
+
+}
