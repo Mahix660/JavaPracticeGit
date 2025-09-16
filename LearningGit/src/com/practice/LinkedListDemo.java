43edb178080219e7a7134bbe20a25aa838d25cd9
Mohit <mahajanmahi729@gmail.com>
2025-09-17 00:26:43 +0530

    Nothing change

--- LearningGit/src/com/practice/LinkedListDemo.java
+++ LearningGit/src/com/practice/LinkedListDemo.java
@@ -4,11 +4,16 @@ public class LinkedListDemo {
 
 	public static void main(String[] args) {
 		
+		
+		
+		
 		for(int i=0;i<5;i++) {
 			System.out.println(i);
 		}
 		
 		System.out.println("hllo world");
+		
+		
 		
 	}
 }
