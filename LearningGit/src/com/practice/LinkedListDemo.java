4e6707074bb7d4dd47e5b66039b54bbc01b9712e
Mohit <mahajanmahi729@gmail.com>
2025-09-18 00:02:34 +0530

    addition program added here

--- LearningGit/src/com/practice/LinkedListDemo.java
+++ LearningGit/src/com/practice/LinkedListDemo.java
@@ -5,13 +5,17 @@ public class LinkedListDemo {
 	public static void main(String[] args) {
 		
 		
+		int a=10;
+		int b =5;
+		int c = a+b;
+		System.out.println(c);
 		
 		
 		for(int i=0;i<5;i++) {
 			System.out.println(i);
 		}
 		
-		System.out.println("hllo world");
+//		System.out.println("hllo world");
 		
 		
 		
