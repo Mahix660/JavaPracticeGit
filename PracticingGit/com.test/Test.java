d5dae721312b91065f4878bcec80857aaaea4c02
Mohit <mahajanmahi729@gmail.com>
2025-09-17 22:33:53 +0530

    added SOP statement

--- PracticingGit/com.test/Test.java
+++ PracticingGit/com.test/Test.java
@@ -2,9 +2,11 @@
 public class Test {
 	public static void main(String[] args) {
 		
+	System.out.println("Printing from 1 to 10 numbers");
 		for(int i=1;i<=10;i++) {
 			System.out.println(i);
 		}
+		
 	}
 
 }
